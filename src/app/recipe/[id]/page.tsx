e319055077aca6e376ee798877cde5c1fd9db707
Mucyo kevin <mucyocyeve22@gmail.com>
2025-07-14 20:47:31 +0200

    commit at 8:47pm

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,6 +1,6 @@
 import RecipeDetails from '../../../pages/RecipeDetails';
 
-export default async function RecipeDetailsPage({ params }: { params: { id: string } }) {
+export default async function RecipeDetailsPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   return <RecipeDetails id={id} />;
 } 
